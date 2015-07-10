979b4dc312958295822334364c2d086823cbc163
Odin <odinuv@gmail.com>
2015-07-10 16:36:05 +0200

    fix: disable warning check which does not work fix: make config file optional

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,16 @@
 library(testthat)
 
-source("config.R")
+# default values
+DATA_DIR = 'tests/data'
+
+# override with config if any
+if (file.exists("config.R")) {
+    source("config.R")
+}
+
+# override with environment if any
+if (nchar(Sys.getenv("DATA_DIR")) > 0) {
+    DATA_DIR <- Sys.getenv("DATA_DIR")  
+}
+
 test_check("keboola.r.transformation")
