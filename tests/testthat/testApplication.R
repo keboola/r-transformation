a1bac18eb6767a7d82d0081fe45ce2f62300b19f
Odin <odinuv@gmail.com>
2016-01-26 15:12:15 +0100

    fix: stupid typo

--- tests/testthat/testApplication.R
+++ tests/testthat/testApplication.R
@@ -1,10 +1,10 @@
 test_that("validate", {
-    app <- RTransformation$new(file.path(DATA_DIR, '01'))
+    app <- RTransformation$new(file.path(KBC_DATA_DIR, '01'))
     app$readConfig()
     app$run()
     
-    expect_true(file.exists(file.path(DATA_DIR, '01', 'out', 'tables')))
-    data <- read.csv(file.path(DATA_DIR, '01', 'out', 'tables', 'sample.csv'))
+    expect_true(file.exists(file.path(KBC_DATA_DIR, '01', 'out', 'tables')))
+    data <- read.csv(file.path(KBC_DATA_DIR, '01', 'out', 'tables', 'sample.csv'))
     expect_equal(
         data[['funkyNumber']]^3,
         data[['biggerFunky']]
