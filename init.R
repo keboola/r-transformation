6d3b7c8d5ee163357eaad6dfd8c5446fa2a7501e
Odin <odinuv@gmail.com>
2016-05-13 10:50:52 +0200

    debug build

--- init.R
+++ init.R
@@ -4,4 +4,10 @@ library('devtools')
 devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
 devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
 # install the transformation application which is present in local directory
+
+ip <- as.data.frame(installed.packages()[,c(1,3:4)])
+rownames(ip) <- NULL
+ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+print(ip, row.names=FALSE)
+
 devtools::install('.') 
